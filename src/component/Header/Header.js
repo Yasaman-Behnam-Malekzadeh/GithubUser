d5df99f1494858f8679c31781247b3fa56d3df8a
Yasaman-Behnam-Malekzadeh <malekzadeh.yasi@yahoo.com>
2024-05-15 15:59:34 +0200

    add tailwind and use it in header

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,20 +1,24 @@
-import React from 'react'
+import React from "react";
+import { GrGithub } from "react-icons/gr";
+import { BsList } from "react-icons/bs";
 
 const Header = () => {
-    return (
-        <div className="navbar bg-dark">
-            <div className="text-light container-fluid h5 d-flex justify-content-between mt-2">
-                <div >
-                    <i className="fab fa-github"/>
-                    <span className="ml-2">Github Users</span> 
-                </div>
+  return (
+    <div className="navbar bg-gray-100 border-bottom flex items-center py-3 h-16">
+      <div className="flex w-full">
+        <BsList
+          size={20}
+          className="border-slate-300 border rounded-md text-slate-500 flex h-8 w-8 p-1"
+        />
+        <GrGithub size={35} className="ml-3" />
+        <input
+          type="text"
+          className="border h-8 border-slate-300 text-slate-400 ml-2 w-10/12 rounded-md bg-gray-100 pl-2"
+          placeholder="search"
+        />
+      </div>
+    </div>
+  );
+};
 
-                <div className="d-flex ">
-                    <a href="https://github.com/" className="text-light small text-decoration-none">Github Link</a>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default Header
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
 
-import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
